41c1cea23a28e7d4d95f608b1be06cb6956dbecc
Guillermo López García <lopezgarciaguillermo@live.com>
2022-05-16 00:50:12 +0200

    feat(login.tsx): add login page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,36 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Henry's Polls</title>
+        <meta name='description' content='Polls project, all thanks to henry' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Component {...pageProps} />
+      <footer className={styles.footer}>
+        <div>
+          <a
+            href='https://es.wikipedia.org/wiki/Mr._Robot'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Powered by{' '}
+          </a>
+        </div>
+        <div>
+          <span className={styles.logo}>
+            <Image src='/mrrobot.png' alt='' width={72} height={52} />
+          </span>
+        </div>
+      </footer>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/login.tsx
@@ -0,0 +1,59 @@
+import type { NextPage } from 'next';
+import styles from '../styles/Login.module.css';
+
+import {
+  Checkbox,
+  Grid,
+  TextField,
+  FormControlLabel,
+  Paper,
+  Button,
+} from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/core';
+import { Theme } from '../styles/theme';
+
+const Login: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Welcome to Login</h1>
+        <ThemeProvider theme={Theme}>
+          <Paper>
+            <Grid
+              container
+              spacing={3}
+              direction={'column'}
+              justifyContent={'center'}
+              alignItems={'center'}
+            >
+              <Grid item xs={12}>
+                <TextField label='Username'></TextField>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField label='Password' type={'password'}></TextField>
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={true}
+                      onChange={() => {}}
+                      label={'Keep me logged in'}
+                      inputProps={{ 'aria-label': 'primary checkbox' }}
+                    />
+                  }
+                  label='Keep me logged in'
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Button fullWidth> Login </Button>
+              </Grid>
+            </Grid>
+          </Paper>
+        </ThemeProvider>
+      </main>
+    </div>
+  );
+};
+
+export default Login;
