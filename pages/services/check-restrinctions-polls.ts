edda2fac58d679559c04d656a5199e8b07d220dd
Guillermo López García <lopezgarciaguillermo@live.com>
2022-05-15 20:55:08 +0200

    feat: add initial code to set a architecture in front-end

---
+++ pages/services/check-restrinctions-polls.ts
@@ -0,0 +1,7 @@
+/**
+ *
+ *
+ * @export
+ */
+// TODO: add code
+export function checks() {}
---
+++ pages/types/User.ts
@@ -0,0 +1,10 @@
+/**
+ *
+ *
+ * @export
+ * @interface User
+ */
+export interface User {
+  id: number;
+  name: string;
+}
