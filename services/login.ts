aad46be5b1d0823133363c0084acc4acd8b80627
Guillermo López García <lopezgarciaguillermo@live.com>
2022-06-05 01:48:15 +0200

    feat: add session with iron-session

--- services/login.ts
+++ services/login.ts
@@ -1,5 +1,9 @@
 import { PUBLIC_ROUTES } from './routes';
-
+import { Octokit } from 'octokit';
+import { withIronSessionApiRoute } from 'iron-session/next';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { sessionOptions } from './session';
+import { User } from '../types/User';
 const PATHS: Array<string> = PUBLIC_ROUTES.map(({ path }) => path);
 
 export const availablePathWithoutLogin = (path: string): boolean => {
@@ -9,3 +13,24 @@ export const availablePathWithoutLogin = (path: string): boolean => {
 export const isSigned = (): boolean => {
   return true;
 };
+
+const octokit = new Octokit();
+
+export const login = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { username } = await req.body;
+
+  try {
+    const {
+      data: { login },
+    } = await octokit.rest.users.getByUsername({ username });
+
+    const user = { login, username } as User;
+    req.session.user = user;
+    await req.session.save();
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: (error as Error).message });
+  }
+};
+
+export default withIronSessionApiRoute(login, sessionOptions);
--- types/User.ts
+++ types/User.ts
@@ -7,4 +7,7 @@
 export interface User {
   id: number;
   name: string;
+  username: string;
+  password: string;
+  login: boolean;
 }
